2bde1449caed320bb6afecd7711747eccaa6c4f9
AnastAnt <141515437+AnastAnt@users.noreply.github.com>
2024-04-25 21:50:11 +0200

    hook

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
-import { Navigation } from "../Navigation/Navigation";
-import { UserMenu } from "../UserMenu/UserMenu";
-import { AuthNav } from "../AuthNav/AuthNav";
-import { useAuth } from "../../hooks/useAuth";
-import css from "./AppBar.module.css";
+import { Navigation } from "../components/Navigation/Navigation";
+import { UserMenu } from "../components/UserMenu/UserMenu";
+import { AuthNav } from "../components/AuthNav/AuthNav";
+import { useAuth } from "../hooks/useAuth";
+import css from "./AppBar/AppBar.module.css";
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
